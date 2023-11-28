292e9f29273e83e9b89787139caf87ba22ee2a48
Shivam Rana <86354964+shivamranaaa@users.noreply.github.com>
2023-11-28 07:25:20 +0530

    Update mainreg.py

--- mainreg.py
+++ mainreg.py
@@ -132,7 +132,7 @@ with mlflow.start_run():
 
     ## For Remote server only(DAGShub)
 
-    remote_server_uri="https://dagshub.com/shivamrana12321/adsad.mlflow"
+    remote_server_uri="https://dagshub.com/shivamrana12321/house-price-prediction-with-mlflow.mlflow"
     mlflow.set_tracking_uri(remote_server_uri)
 
     tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
